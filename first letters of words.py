ad839c934e732bded5ceb9654699ae870c1bd771
Steve Laurensius <124287156+stevelaurensius@users.noreply.github.com>
2024-04-07 23:02:12 +0700

    Delete first letters of words.py

--- first letters of words.py
+++
@@ -1,9 +0,0 @@
-# Please write a program which asks the user to type in a sentence. The program then
-# prints out the first letter of each word in the sentence, each letter on a separate line.
-
-input_string = input('Please type in a sentence: ')
-dictionary = input_string.split()
-
-for i in range(len(dictionary)):
-    print(dictionary[i][0])
-    i += 1
