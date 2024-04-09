0636d6ad3a914141ed8f75a6634d74cd7d72d472
stevelaurensius <steve.laurensius+github@gmail.com>
2024-04-09 20:10:16 +0700

    1st version

---
+++ Part 3 - Defining Functions/mean.py
@@ -0,0 +1,8 @@
+# Please write a function named mean, which takes three integer arguments.
+# The function should print out the arithmetic mean of the three arguments.
+def mean(x, y, z):
+    print((x + y + z) / 3)
+
+
+if __name__ == "__main__":
+    mean(1, 2, 3)
