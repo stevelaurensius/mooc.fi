95dc0095fec5ac49437b7cf8551036df54f6b9a2
stevelaurensius <steve.laurensius+github@gmail.com>
2024-04-09 19:42:57 +0700

    1st version

---
+++ Part 3 - Defining Functions/seven_brothers.py
@@ -0,0 +1,24 @@
+# Please write a function named seven_brothers. When the
+# function is called, it should print out the names of the seven
+# brothers in alphabetical order, as in the example below.
+#
+# Aapo
+# Eero
+# Juhani
+# Lauri
+# Simeoni
+# Timo
+# Tuomas
+
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+
+
+if __name__ == "__main__":
+    seven_brothers()
