a54b8a02ddff40adc01c92ca62ab6bc599afce6f
stevelaurensius <steve.laurensius+github@gmail.com>
2024-04-11 22:00:01 +0700

    1st version

---
+++ Part 4 - Lists/the_length_of_a_list.py
@@ -0,0 +1,10 @@
+# Please write a function named length which takes a list as its argument and
+# returns the length of the list.
+def length(x):
+    return len(x)
+
+
+if __name__ == "__main__":
+    my_list = [3, 6, -4]
+    result = length(my_list)
+    print(result)
