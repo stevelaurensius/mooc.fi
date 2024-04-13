854edbf1fe5de6ee82390cc26df57df2faac38e0
stevelaurensius <steve.laurensius+github@gmail.com>
2024-04-13 17:17:46 +0700

    1st version

---
+++ Part 4 - More Strings and Lists/everything_reversed.py
@@ -0,0 +1,17 @@
+# Please write a function named everything_reversed, which takes a list of
+# strings as its argument. The function returns a new list with all of the items on
+# the original list reversed. Also the order of items should be reversed on the new
+# list.
+#
+# An example of how the function should work:
+# my_list = ["Hi", "there", "example", "one more"]
+# new_list = everything_reversed(my_list)
+# print(new_list)
+def everything_reversed(x):
+    return [i[::-1] for i in x[::-1]]
+
+
+if __name__ == '__main__':
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
