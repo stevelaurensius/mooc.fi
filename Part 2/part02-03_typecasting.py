6f690a5b4e6f62577a213047deb2b2d7d04ec0d5
stevelaurensius <steve.laurensius+github@gmail.com>
2024-05-12 13:31:52 +0700

    1st version

---
+++ Part 2/part02-03_typecasting.py
@@ -0,0 +1,5 @@
+angka = float(input('Please type in a number:'))
+bilangan_bulat = int(angka)
+desimal = angka - bilangan_bulat
+print(f'Integer part: {bilangan_bulat}')
+print(f'Decimal part: {desimal}')
