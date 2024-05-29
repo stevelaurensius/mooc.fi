26c794e7f43e3c5413dcd14de57b47d7beaa4860
stevelaurensius <steve.laurensius+github@gmail.com>
2024-05-29 18:21:54 +0700

    1st version

---
+++ Part 8/part08-04_shopping_list.py
@@ -0,0 +1,30 @@
+class ShoppingList:
+    def __init__(self):
+        self.products = []
+
+    def number_of_items(self):
+        return len(self.products)
+
+    def add(self, product: str, number: int):
+        self.products.append((product, number))
+
+    def item(self, n: int):
+        return self.products[n - 1][0]
+
+    def amount(self, n: int):
+        return self.products[n - 1][1]
+
+
+def total_units(my_list: ShoppingList):
+    amount = 0
+    for i in range(1, my_list.number_of_items()+1):
+        amount += my_list.amount(i)
+    return amount
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+    print(total_units(my_list))
