c06f50a5bc77fc93a2e68e448e8c53abefd58805
stevelaurensius <steve.laurensius+github@gmail.com>
2024-06-03 19:03:32 +0700

    1st version

---
+++ Part 9/part09-02_passing_submissions.py
@@ -0,0 +1,24 @@
+class ExamSubmission:
+    def __init__(self, examinee: str, points: int):
+        self.examinee = examinee
+        self.points = points
+
+    def __str__(self):
+        return f'ExamSubmission (examinee: {self.examinee}, points: {self.points})'
+
+
+def passed(submissions: list, lowest_passing: int):
+    result = [x for x in submissions if x.points >= lowest_passing]
+    return result
+
+
+if __name__ == "__main__":
+    s1 = ExamSubmission("Peter", 12)
+    s2 = ExamSubmission("Pippa", 19)
+    s3 = ExamSubmission("Paul", 15)
+    s4 = ExamSubmission("Phoebe", 9)
+    s5 = ExamSubmission("Persephone", 17)
+
+    passes = passed([s1, s2, s3, s4, s5], 15)
+    for passing in passes:
+        print(passing)
