078d3ef758bf625804d65ba01c0fb4f64f9f9078
stevelaurensius <steve.laurensius+github@gmail.com>
2024-06-03 18:42:33 +0700

    1st version

---
+++ Part 9/part09-01_fastest_car.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+
+def fastest_car(cars):
+    fastest = max(cars, key=lambda car: car.top_speed)
+    return fastest.make
+
+
+if __name__ == "__main__":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
