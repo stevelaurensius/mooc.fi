edb6582c5671d01044585ee25c3b5a079c3a9da3
stevelaurensius <steve.laurensius+github@gmail.com>
2024-06-14 20:56:56 +0700

    1st version

---
+++ Part 10/part10-01_laptop_computer.py
@@ -0,0 +1,30 @@
+class Computer:
+    def __init__(self, model: str, speed: int):
+        self.__model = model
+        self.__speed = speed
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def speed(self):
+        return self.__speed
+
+
+class LaptopComputer(Computer):
+    def __init__(self, model, speed, weight):
+        super().__init__(model, speed)
+        self.__weight = weight
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return f'{self.model}, {self.speed} MHz, {self.weight} kg'
+
+
+if __name__ == '__main__':
+    laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
+    print(laptop)
