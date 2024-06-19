2a13cf476f0f559cab2bdada880a9026ab74f03c
stevelaurensius <steve.laurensius+github@gmail.com>
2024-06-19 15:45:16 +0700

    1st version

---
+++ Part 11/part11-01_square_roots.py
@@ -0,0 +1,11 @@
+import math
+
+
+def square_roots(numbers: list):
+    return [math.isqrt(x) for x in numbers]
+
+
+if __name__ == '__main__':
+    lines = square_roots([1, 2, 3, 4])
+    for line in lines:
+        print(line)
