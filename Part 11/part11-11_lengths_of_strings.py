fad01b99e4446f497c1bddb156410d6362e4e088
stevelaurensius <steve.laurensius+github@gmail.com>
2024-06-19 16:52:55 +0700

    1st version

---
+++ Part 11/part11-11_lengths_of_strings.py
@@ -0,0 +1,9 @@
+def lengths(strings: list):
+    return {word: len(word) for word in strings}
+
+
+if __name__ == '__main__':
+    word_list = ["once", "upon", "a", "time", "in"]
+
+    word_lengths = lengths(word_list)
+    print(word_lengths)
